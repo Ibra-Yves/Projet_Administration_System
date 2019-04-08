225c2917a57408ea5d520648a1dd6d2e9de13cbf
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2019-04-08 15:56:22 +0200

    Create send_date.php

---
+++ Website/b2b/send_date.php
@@ -0,0 +1,33 @@
+
+<?php
+$servername = "51.77.203.64";
+$username = "root";
+$password = "root";
+$dbname = "user";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$nom = $_POST['nom'];
+$age = $_POST['age'];
+
+$sql = "INSERT INTO personne (nom, age)
+VALUES ('$nom', '$age')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+$conn->close();
+
+header('Location: ./index.php');
+die();
+?>
